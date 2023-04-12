966b0108b09e2a2e33f9dfeb2876a32a704bf221
Steen Krøyer <stek@via.dk>
2023-04-12 13:09:24 +0000

    Hardened pipeline scripts

--- pipeline/build_firmware_files.sh
+++ pipeline/build_firmware_files.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
+set -e
 
+rm -f firmware.*
 cd target
+rm -rf build
 /usr/bin/cmake --no-warn-unused-cli -DCMAKE_VERBOSE_MAKEFILE:STRING=ON -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/avr-gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/avr-g++ -S. -Bbuild
 /usr/bin/cmake --build build --config Release --target create_firmware_files --
 mv build/firmware.* ..
--- pipeline/config_build_run_test.sh
+++ pipeline/config_build_run_test.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
+set -e
 
 cd test
-rm -rf build
+rm -rf build test_result.xml
 /usr/bin/cmake -Wno-dev --no-warn-unused-cli -DDOWNLOAD_EXTRACT_TIMESTAMP=TRUE -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S. -Bbuild
 /usr/bin/cmake --build build --config Debug --target all --
 
