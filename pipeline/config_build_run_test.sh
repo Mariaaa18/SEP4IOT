f474f4ae8c573b33b7a257f3d939ea9a74ccdf6c
Steen Krøyer <stek@via.dk>
2023-04-12 13:51:12 +0000

    Tweaked test result output

--- pipeline/config_build_run_test.sh
+++ pipeline/config_build_run_test.sh
@@ -2,12 +2,12 @@
 set -e
 
 cd test
-rm -rf build test_result.xml
+rm -rf build test_result.xml ../test_result.xml
 /usr/bin/cmake -Wno-dev --no-warn-unused-cli -DDOWNLOAD_EXTRACT_TIMESTAMP=TRUE -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S. -Bbuild
 /usr/bin/cmake --build build --config Debug --target all --
 
 ./build/iot_demo_test --gtest_output="xml:temp.xml"
-sed 's/\/workspaces\/iot-demo\///g' < temp.xml > test_result.xml
+sed 's/\/workspaces\/iot-base\///g' < temp.xml > ../test_result.xml
 rm temp.xml
 #ctest --output-junit ../test_result.xml --test-dir build
 exit 0
