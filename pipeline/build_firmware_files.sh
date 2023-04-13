fd8d97886462873dce72d4abbad0cd9d23a2d328
Steen Krøyer <stek@via.dk>
2023-04-13 07:10:30 +0000

    Small tweaks to compile options

--- pipeline/build_firmware_files.sh
+++ pipeline/build_firmware_files.sh
@@ -4,8 +4,7 @@ set -e
 rm -f firmware.*
 cd target
 rm -rf build
-/usr/bin/cmake --no-warn-unused-cli -DCMAKE_VERBOSE_MAKEFILE:STRING=ON -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/avr-gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/avr-g++ -S. -Bbuild
-/usr/bin/cmake --build build --config Release --target create_firmware_files --
+/usr/bin/cmake --no-warn-unused-cli -DCMAKE_VERBOSE_MAKEFILE:STRING=ON -DCMAKE_BUILD_TYPE:STRING=RELEASE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/avr-gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/avr-g++ -S. -Bbuild
+/usr/bin/cmake --build build --config RELEASE --target create_firmware_files --
 mv build/firmware.* ..
 exit 0
-
