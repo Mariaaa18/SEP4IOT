10545160ee0baadd21ddb1237e38f92d52bc2b9a
Steen Kroyer <stek@via.dk>
2023-04-14 12:23:57 +0000

    Sigh!

--- pipeline/config_build_run_test.sh
+++ pipeline/config_build_run_test.sh
@@ -6,7 +6,7 @@ rm -rf build test_result.xml ../test_result.xml
 /usr/bin/cmake -Wno-dev --no-warn-unused-cli -DDOWNLOAD_EXTRACT_TIMESTAMP=TRUE -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S. -Bbuild
 /usr/bin/cmake --build build --config Debug --target all --
 
-./build/iot_demo_test --gtest_output="xml:temp.xml"
+./build/iot_base_test --gtest_output="xml:temp.xml"
 sed 's/\/workspaces\/iot-base\///g' < temp.xml > ../test_result.xml
 rm temp.xml
 #ctest --output-junit ../test_result.xml --test-dir build
