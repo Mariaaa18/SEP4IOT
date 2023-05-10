c4951e92dbe9078066ab244968065196dd1eb253
Dércio Fernandes <315236@via.dk>
2023-05-10 08:55:35 +0000

    More Changes to the Tests

--- test/src/hello_test.cpp
+++ test/src/hello_test.cpp
@@ -4,14 +4,14 @@
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
+  EXPECT_STRNE("skdhasjdjas", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
 
 TEST(HelloTest2, BasicAssertions) {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "worl");
+  EXPECT_STRNE("hello", "asndasllkasd");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
