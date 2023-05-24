a1d0af03dd2b8fbbf9d11bf0962deda4d21aa844
Dércio Fernandes <315236@via.dk>
2023-05-24 11:50:57 +0000

    feature;testbase

---
+++ test/src/test_event_group_await_bits.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+#include "fff/fff.h"
+#include "FreeRTOS_defs/FreeRTOS_FFF_MocksDeclaration.h"
+
+extern "C"
+{
+#include <controllerSender.h>
+#include <serial.h>
+}
+
+FAKE_VOID_FUNC(run_coTow)
+FAKE_VOID_FUNC(run_coTow)
+FAKE_VOID_FUNC(run_coTow)
+
